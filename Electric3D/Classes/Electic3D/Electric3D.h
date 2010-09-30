55a914f16ef6b4801d4deee89b02daa231529f0d
Bert McDowell <bertmcdowell@gmail.com>
2010-09-30 20:29:11 +0000

    WIP changes
    git-svn-id: file:///Users/robertmcdowell/Documents/Mirrors/Electric3D/trunk@9 2e2d8f00-7a33-48b8-8dfb-cfb7de531507

---
+++ Electric3D/Classes/Electic3D/Electric3D.h
@@ -0,0 +1,15 @@
+/*
+ *  Electric3D.h
+ *  Electric3D
+ *
+ *  Created by Robert McDowell on 30/09/2010.
+ *  Copyright 2010 Electric TopHat Ltd. All rights reserved.
+ *
+ */
+
+#if !defined(__Electric3D_h__)
+#define __Electric3D_h__
+
+
+
+#endif
